6c3896db1780af52997fbb1924758e415ef58e60
TheBornTurtle <bornturtle@outlook.com>
2014-11-25 22:01:03 +0000

    Made a window

--- src/main/java/turtle/game/rmagus/Base.java
+++ src/main/java/turtle/game/rmagus/Base.java
@@ -1,5 +1,60 @@
 package turtle.game.rmagus;
 
-public class Base {
+import java.awt.Canvas;
+import java.awt.Dimension;
+import javax.swing.JFrame;
 
+public class Base extends Canvas implements Runnable {
+
+	public static int HEIGHT = 480;
+	public static int WIDTH = HEIGHT / 9 * 16;
+	public static int SCALE = 3;
+	
+	private static final long serialVersionUID = 1L;
+	private Thread thread;
+	private boolean running = false;
+	private JFrame frame;
+
+	public static void main(String[] args) {
+		Base game = new Base();
+		game.frame.setResizable(true);
+		game.frame.setTitle("Retro Magus | Alpha | v. 1.0");
+		game.frame.add(game);
+		game.frame.pack();
+		game.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		game.frame.setLocationRelativeTo(null);
+		game.frame.setVisible(true);
+		game.start();
+
+	}
+
+	
+	public Base() {
+		Dimension size = new Dimension(WIDTH * SCALE, HEIGHT * SCALE);
+		setPreferredSize(size);
+
+		frame = new JFrame();
+	}
+
+	public synchronized void start() {
+		running = true;
+		thread = new Thread(this, "Game");
+		thread.start();
+	}
+
+	public synchronized void stop() {
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public void run() {
+		while (running) {
+		}
+	}
 }
