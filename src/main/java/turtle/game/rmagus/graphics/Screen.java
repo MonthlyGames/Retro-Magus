c87d1310c2a49eced1eb26ec8dca7e0ac34b6c52
TheBornTurtle <bornturtle@outlook.com>
2014-11-27 18:30:29 +0000

    Added a randomly generating map

--- src/main/java/turtle/game/rmagus/graphics/Screen.java
+++ src/main/java/turtle/game/rmagus/graphics/Screen.java
@@ -1,14 +1,23 @@
 package turtle.game.rmagus.graphics;
 
+import java.util.Random;
+
 public class Screen {
 	private int width, height;
 
 	public int[] pixels;
+	public int[] tiles = new int[64 * 64];
+
+	private Random random = new Random();
 
 	public Screen(int width, int height) {
 		this.width = width;
 		this.height = height;
 		pixels = new int[width * height];
+
+		for (int i = 0; i < 64 * 64; i++) {
+			tiles[i] = random.nextInt(0xCBBBBC);
+		}
 	}
 
 	public void clear() {
@@ -24,7 +33,8 @@ public class Screen {
 			for (int x = 0; x < width; x++) {
 				if (x < 0 || x >= width)
 					break;
-				pixels[x + y * width] = 0xFF00FF;
+				int tileIndex = (x >> 4) + (y >> 4) * 64;
+				pixels[x + y * width] = tiles[tileIndex];
 			}
 		}
 	}
